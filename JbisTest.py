27605a355526c9898a45ede32f1dc3709bb9bbc1
nicolasBeraud <nicolas.beraud@univ-grenoble-alpes.fr>
2021-09-28 17:39:08 +0200

    add unit test

--- JbisTest.py
+++ JbisTest.py
@@ -55,5 +55,28 @@ class JbisTest(unittest.TestCase):
             self.assertTrue(os.path.exists(name))
             self.assertTrue(compare(name, "result/" + name))
 
+    def test_tourne_AB(self):
+        self.classic_test("u_incl_bra.APT", False, False, 0)
+
+        self.classic_test("u_incl_braA0.APT", True, False, 0)
+        self.classic_test("u_incl_braA1.APT", True, False, 90)
+        self.classic_test("u_incl_braA2.APT", True, False, 180)
+        self.classic_test("u_incl_braA3.APT", True, False, 270)
+
+        self.classic_test("u_incl_braAB0.APT", True, True, 90)
+        self.classic_test("u_incl_braAB1.APT", True, True, -90)
+        self.classic_test("u_incl_braAB3.APT", True, True, -90)
+
+
+    def classic_test(self, file_name: str, with_A, with_B, default_angle):
+        file = Jbi(file_name, "DEMO", with_A, with_B, default_angle)
+        print(file.files)
+        for name in file.files:
+            print(name)
+            self.assertTrue(os.path.exists(name))
+            self.assertTrue(compare(name, "result/" + name))
+
+
+
 if __name__ == '__main__':
     unittest.main()
--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 from Jbis import Jbi
-temp = Jbi("u_angle.APT", "DEMO", False)
+
+Jbi("u_incl_bra.APT", "DEMO", False, False, 0)
+
+Jbi("u_incl_braA0.APT", "DEMO", True, False, 0)
+Jbi("u_incl_braA1.APT", "DEMO", True, False, 90)
+Jbi("u_incl_braA2.APT", "DEMO", True, False, 180)
+Jbi("u_incl_braA3.APT", "DEMO", True, False, 270)
+
+Jbi("u_incl_braAB0.APT", "DEMO", True, True, 90)
+Jbi("u_incl_braAB1.APT", "DEMO", True, True, -90)
+Jbi("u_incl_braAB3.APT", "DEMO", True, True, -90)
 
 
 
