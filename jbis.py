be24a906a03fc5450b5ec85655212d960f597001
Nicolas Beraud <nicolas.beraud@univ-grenoble-alpes.fr>
2022-04-07 19:05:02 +0200

    change constructor for rhino integration

--- jbis.py
+++ jbis.py
@@ -27,8 +27,9 @@ def define_eol():
         END_OF_LINE = '\r\n'
     elif platform == "darwin":
         print("!!!! no yet implemented !!!!")
-    elif platform == "win32":
+    else:
         END_OF_LINE = '\n'
+
 
 def write_lines(output_path, lines):
     with open(output_path, 'w') as output_file:
@@ -137,7 +138,7 @@ class Jbi:
         return lines
 
     def write_trj_file(self):
-        path = os.path.splitext(self.input_path)[0] + "TRJ" + str(self.trj_part) + "." + FILE_EXTENSION
+        path = self.output_path + "TRJ" + str(self.trj_part) + "." + FILE_EXTENSION
         name = os.path.splitext(path)[0]
         lines = self.intro_trj(len(self.points), name)
 
@@ -173,7 +174,7 @@ class Jbi:
     def write_main_file(self, instructions):
         if len(self.points) > 0:
             self.write_trj_file()
-        name = os.path.splitext(self.input_path)[0]
+        name = self.output_path
         lines = self.intro(0, name)
         lines.append("//INST")
         lines.append("///DATE 2021/02/10 13:51")
@@ -189,92 +190,55 @@ class Jbi:
         lines.append("CALL JOB:FIN")
         lines.append("END")
         write_lines(self.output_path, lines)
-        self.files.append(self.output_path)
+        path = self.output_path + "." + FILE_EXTENSION
+        self.files.append(path)
 
-    def __init__(self, input_path, folder_name, with_B, with_A=False, initial_B=0):
-        # time calculation monitoring
-        tic = time.perf_counter()
+    def __init__(self, folder_name, with_B, with_A=False, initial_B=0, lines=None, output_path="", input_path=""):
+        """
+
+        :param folder_name: folder name for programm on the robot
+        :type folder_name: string
+        :param with_B: true if you want to use B axe
+        :type with_B: bool
+        :param with_A: true if you want to use A axe
+        :type with_A: bool
+        :param initial_B: initial position of the torch on B axes (only used if with_B is true)
+        :type initial_B: float
+        :param lines: APT instruction lines : used if you want to directly send APT instruction line (for example from Rhino)
+        :type lines: list of string
+        :param output_path: path where to sage the restult (must be a file path) only need if you send APT instruction lines
+        :type output_path: string
+        :param input_path: path for the APT file : only need if you do not want to send APT instruction line but read them in a file
+        :type input_path: sting
+        """
         define_eol()
 
         self.with_B = with_B
         self.with_A = with_A
         self.folder_name = folder_name
-        self.input_path = input_path
         self.initial_B = initial_B
         self.previous_B = 0
-        self.output_path = os.path.splitext(input_path)[0] + "." + FILE_EXTENSION
-        print("Input path: " + self.input_path)
-        print("Output path: " + self.output_path)
-        self.files = []
+        if output_path is not None and lines is not None:
+            self.output_path = os.path.splitext(output_path)[0]
+            self.translate_lines(lines)
+        elif input_path is not None:
+            self.output_path = os.path.splitext(input_path)[0]
+            self.read_file(input_path)
+        else:
+            print("!!! error in constructor parameters")
 
-        self.points = []
+        print("Output path: " + self.output_path)
+
+    def read_file(self, input_path):
+        # time calculation monitoring
+        tic = time.perf_counter()
+
+        print("Input path: " + input_path)
 
         if os.path.isfile(input_path):
             with open(input_path, 'r') as file:
                 lines = file.readlines()
-                self.main_instructions = []
-                self.trj_part = 0
-                self.trj_instructions = []
-                rapid = False
-
-                for line_number, line in enumerate(lines):
-                    line = line.rstrip()
-                    if len(line) == 0:
-                        print("Empty line")
-
-                    elif line[0] == "$" and line[1] == "$":
-                        self.trj_instructions.append("'" + line[2:])
-                    else:
-                        line = line.replace(" ", "")
-                        arguments = line.split('/')
-                        instruction = arguments[0]
-
-                        if instruction == "ARC":
-                            argument = arguments[1]
-                            if argument == "ON":
-                                self.trj_instructions.append("ARCON")
-                            elif argument == "OFF":
-                                self.trj_instructions.append("ARCOF")
-                            else:
-                                print("!!! Unknown argument in instruction ARC line " + str(line_number) + ": " + line)
-
-                        elif instruction == "GOTO":
-                            coordinates = arguments[1].split(',')
-                            self.points.append(
-                                Point(float(coordinates[0]), float(coordinates[1]), float(coordinates[2]),
-                                      float(coordinates[3]), float(coordinates[4]), float(coordinates[5])))
-                            if self.with_B:
-                                self.trj_instructions.append("SMOVL C" + str(len(self.points) - 1).zfill(5)
-                                                             + " +MOVJ EC" + str(len(self.points) - 1).zfill(5))
-                            else:
-                                self.trj_instructions.append("MOVL C" + str(len(self.points) - 1).zfill(5))
-                            if rapid:
-                                rapid = False
-                            if len(self.points) > 1999:
-                                self.write_trj_file()
-
-                        elif instruction == "FEEDRATE":
-                            options = arguments[1].split(',')
-                            if options[0] != "MMPS":
-                                print("!!! Unknown unit for instruction FEDRATE in line " + str(
-                                    line_number) + ": " + line)
-                            else:
-                                speed = float(options[1])
-                                self.trj_instructions.append("SPEED V={:.1f}".format(speed))
-                        elif instruction == "WAIT":
-                            timer = float(options[1])
-                            self.trj_instructions.append("TIMER T={:.2f}".format(timer))
-                        elif instruction == "RAPID":
-                            rapid = True
-                            self.trj_instructions.append("SPEED V={:.1f}".format(RAPID_SPEED))
-                        elif instruction == "TASK":
-                            self.trj_instructions.append("DOUT OG#(12) " + arguments[1])
-                        else:
-                            print("!!! Unknown instruction in line " + str(line_number) + ": " + line)
-
-                print("Writing main file")
-                self.write_main_file(self.main_instructions)
-
+                self.translate_lines(lines)
         else:
             print("no corresponding file to: " + input_path)
 
@@ -282,3 +246,68 @@ class Jbi:
         toc = time.perf_counter()
         delay = toc - tic
         print("Calculation time: " + str(toc))
+
+    def translate_lines(self, lines):
+        self.files = []
+        self.points = []
+        self.main_instructions = []
+        self.trj_part = 0
+        self.trj_instructions = []
+        rapid = False
+        for line_number, line in enumerate(lines):
+            line = line.rstrip()
+            if len(line) == 0:
+                print("Empty line")
+
+            elif line[0] == "$" and line[1] == "$":
+                self.trj_instructions.append("'" + line[2:])
+            else:
+                line = line.replace(" ", "")
+                arguments = line.split('/')
+                instruction = arguments[0]
+
+                if instruction == "ARC":
+                    argument = arguments[1]
+                    if argument == "ON":
+                        self.trj_instructions.append("ARCON")
+                    elif argument == "OFF":
+                        self.trj_instructions.append("ARCOF")
+                    else:
+                        print("!!! Unknown argument in instruction ARC line " + str(line_number) + ": " + line)
+
+                elif instruction == "GOTO":
+                    coordinates = arguments[1].split(',')
+                    self.points.append(
+                        Point(float(coordinates[0]), float(coordinates[1]), float(coordinates[2]),
+                              float(coordinates[3]), float(coordinates[4]), float(coordinates[5])))
+                    if self.with_B:
+                        self.trj_instructions.append("SMOVL C" + str(len(self.points) - 1).zfill(5)
+                                                     + " +MOVJ EC" + str(len(self.points) - 1).zfill(5))
+                    else:
+                        self.trj_instructions.append("MOVL C" + str(len(self.points) - 1).zfill(5))
+                    if rapid:
+                        rapid = False
+                    if len(self.points) > 1999:
+                        self.write_trj_file()
+
+                elif instruction == "FEEDRATE":
+                    options = arguments[1].split(',')
+                    if options[0] != "MMPS":
+                        print("!!! Unknown unit for instruction FEDRATE in line " + str(
+                            line_number) + ": " + line)
+                    else:
+                        speed = float(options[1])
+                        self.trj_instructions.append("SPEED V={:.1f}".format(speed))
+                elif instruction == "WAIT":
+                    timer = float(options[1])
+                    self.trj_instructions.append("TIMER T={:.2f}".format(timer))
+                elif instruction == "RAPID":
+                    rapid = True
+                    self.trj_instructions.append("SPEED V={:.1f}".format(RAPID_SPEED))
+                elif instruction == "TASK":
+                    self.trj_instructions.append("DOUT OG#(12) " + arguments[1])
+                else:
+                    print("!!! Unknown instruction in line " + str(line_number) + ": " + line)
+
+        print("Writing main file")
+        self.write_main_file(self.main_instructions)
